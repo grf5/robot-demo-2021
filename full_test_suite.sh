2fd1021754fc81b24b21de8fc8ae908288353baf
Greg Robinson <g.robinson@f5.com>
2020-09-24 23:07:17 -0400

    FIXING ROBOT HOST IP VAIRABLE

--- full_test_suite.sh
+++ full_test_suite.sh
@@ -5,6 +5,7 @@ export PYTHONWARNINGS='ignore:Unverified HTTPS request'
 
 # Test case variables
 export ROBOT_HOST_IP=`hostname -I`
+printf "$ROBOT_HOST_IPGREG"
 
 # BIG-IP Devices
 export PRIMARY_HOSTNAME='ip-10-1-1-5.us-west-2.compute.internal'
